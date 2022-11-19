47b38a2a336f50b72b232cee4f39a64d040db220
Ofir Levy <ofirly@mta.ac.il>
2022-11-19 02:16:04 +0200

    updated print on second job

--- ofir.java
+++ ofir.java
@@ -1,6 +1,6 @@
 public class ofir{
     public static void main(String[] args){
         for(int i=1; i<=10; i++)
-            System.out.println("Ofir..." + i);
+            System.out.println("Updated print..." + i);
     }
 }
